30f3e31f36bef73078277ace2bea7c091192f015
vishalpurandare <vishalpurandare@gmail.com>
2015-10-30 21:48:25 -0700

    test

--- MultiDescriptorProject/src/com/mult/entry/Application.java
+++ MultiDescriptorProject/src/com/mult/entry/Application.java
@@ -3,7 +3,8 @@ package com.mult.entry;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Multimedia Project");
 
 	}
 
