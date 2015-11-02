487a29b442403d34a17b3369556552417d1dfb7b
vishalpurandare <vishalpurandare@gmail.com>
2015-11-01 18:18:47 -0800

    Calculated macro blocks

--- MultiDescriptorProject/src/com/mult/entry/VideoDescriptorEntry.java
+++ MultiDescriptorProject/src/com/mult/entry/VideoDescriptorEntry.java
@@ -3,6 +3,8 @@ package com.mult.entry;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import com.mult.core.video.MotionDescriptor;
 import com.mult.util.Constants;
@@ -15,11 +17,22 @@ public class VideoDescriptorEntry {
 		File[] directoryListing = dirVideo.listFiles();
 		
 		//currently only working on one video file, later do it for all files in directoryListings
-		File currFile = directoryListing[1];
+		File currFile = directoryListing[0];
 		
 		MotionDescriptor mObj = MotionDescriptor.getInstance();
 		try {
-			mObj.generateVideoFrames(currFile);
+			//(1) Divide The Video into frames
+			List<int[]> framesComponents = mObj.generateVideoFrames(currFile);
+			
+			//(2) MPEG Based Motion-Compensation technique
+			// (2.1) Divide each Frame into Macro-Block
+			for (int[] compTempArr : framesComponents) {
+				//Will return blocks for each of the frame (image), 15x15 block, 192 for each red, green and blue
+				Map<String, List<int[][]>> macroBlocksMap = mObj.getBlocksFromImageComponents(
+						Constants.WIDTH, Constants.HEIGHT, compTempArr);
+				
+			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
