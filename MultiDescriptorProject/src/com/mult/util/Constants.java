82564732e8a7fd21653a7e592228010bf181a0c6
Sanmukh Lodha <sanmukhlodha@gmail.com>
2015-11-20 00:34:39 -0800

    Color brightness difference descriptor done

--- MultiDescriptorProject/src/com/mult/util/Constants.java
+++ MultiDescriptorProject/src/com/mult/util/Constants.java
@@ -10,23 +10,29 @@ public class Constants {
 	public static final int DESC_IMG_HEIGHT = 50;
 	public static final int NO_OF_FRAMES = 150;
 	public static final int AUDIO_SAMPLES_COUNT = 331081;
-
+	
 	
 	//Vishal: Constants
 	public static final String LINE_SEPARATOR = System
 			.getProperty("line.separator");
 	public static final String FILE_PATH_SEPARATOR = System
 			.getProperty("file.separator");
-	public static final String VIDEO_PATH_VISHAL_PC = "C:" + FILE_PATH_SEPARATOR + "Data"
-			+ FILE_PATH_SEPARATOR + "imp-Data" + FILE_PATH_SEPARATOR
-			+ "USC Data" + FILE_PATH_SEPARATOR + "Courses-Fall 2015"
-			+ FILE_PATH_SEPARATOR + "CSCI576-Multimedia System Design"
-			+ FILE_PATH_SEPARATOR + "Final-Project" + FILE_PATH_SEPARATOR
+	
+	public static final String VIDEO_PATH_SANMUKH_PC = "F:" + FILE_PATH_SEPARATOR + "Fall '15"
+			+ FILE_PATH_SEPARATOR + "CS 576" + FILE_PATH_SEPARATOR
+			+ "Final Project" + FILE_PATH_SEPARATOR 
 			+ "video_samples";
 	
 	public static final String SERIALIZED_FILE_PATH = "C:" + FILE_PATH_SEPARATOR + "Data" + FILE_PATH_SEPARATOR + "serializedObj.ser";
 	public static final int WIDTH = 240;
 	public static final int HEIGHT = 180;
 	public static final int MACRO_BLOCK_SIZE = 15;
+	
+	public static final String VIDEO_PATH_VISHAL_PC = "C:" + FILE_PATH_SEPARATOR + "Data"
+			+ FILE_PATH_SEPARATOR + "imp-Data" + FILE_PATH_SEPARATOR
+			+ "USC Data" + FILE_PATH_SEPARATOR + "Courses-Fall 2015"
+			+ FILE_PATH_SEPARATOR + "CSCI576-Multimedia System Design"
+			+ FILE_PATH_SEPARATOR + "Final-Project" + FILE_PATH_SEPARATOR
+			+ "video_samples";
 	
 }
