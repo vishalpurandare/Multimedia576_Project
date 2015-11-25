eb8e0991ffcd54aa63ba53e9e4f6640e7d66ceb6
Sanmukh Lodha <sanmukhlodha@gmail.com>
2015-11-24 17:26:25 -0800

    Added Sanmukh PC constants

--- MultiDescriptorProject/src/com/mult/util/Constants.java
+++ MultiDescriptorProject/src/com/mult/util/Constants.java
@@ -18,10 +18,7 @@ public class Constants {
 	public static final String FILE_PATH_SEPARATOR = System
 			.getProperty("file.separator");
 	
-	public static final String VIDEO_PATH_SANMUKH_PC = "F:" + FILE_PATH_SEPARATOR + "Fall '15"
-			+ FILE_PATH_SEPARATOR + "CS 576" + FILE_PATH_SEPARATOR
-			+ "Final Project" + FILE_PATH_SEPARATOR 
-			+ "video_samples";
+	
 	
 	public static final String SERIALIZED_FILE_PATH = "C:" + FILE_PATH_SEPARATOR + "MultimediaData" +  FILE_PATH_SEPARATOR + "Database" + FILE_PATH_SEPARATOR;
 	public static final String SERIALIZED_FILE_PATH_TEST = "C:" + FILE_PATH_SEPARATOR + "MultimediaData" +  FILE_PATH_SEPARATOR + "TestData" + FILE_PATH_SEPARATOR;
@@ -36,11 +33,21 @@ public class Constants {
 			+ FILE_PATH_SEPARATOR + "Final-Project" + FILE_PATH_SEPARATOR
 			+ "video_samples";
 	
+	public static final String VIDEO_PATH_SANMUKH_PC = "F:" + FILE_PATH_SEPARATOR + "Fall '15"
+			+ FILE_PATH_SEPARATOR + "CS 576" + FILE_PATH_SEPARATOR
+			+ "Final Project" + FILE_PATH_SEPARATOR 
+			+ "video_samples";
+	
 	public static final String AUDIO_PATH_VISHAL_PC = "C:" + FILE_PATH_SEPARATOR + "Data"
 			+ FILE_PATH_SEPARATOR + "imp-Data" + FILE_PATH_SEPARATOR
 			+ "USC Data" + FILE_PATH_SEPARATOR + "Courses-Fall 2015"
 			+ FILE_PATH_SEPARATOR + "CSCI576-Multimedia System Design"
 			+ FILE_PATH_SEPARATOR + "Final-Project" + FILE_PATH_SEPARATOR
+			+ "audio_wav";
+	
+	public static final String AUDIO_PATH_SANMUKH_PC = "F:" + FILE_PATH_SEPARATOR + "Fall '15"
+			+ FILE_PATH_SEPARATOR + "CS 576" + FILE_PATH_SEPARATOR
+			+ "Final Project" + FILE_PATH_SEPARATOR 
 			+ "audio_wav";
 	
 	public static final String TEST_PATH_VISHAL_PC = "C:" + FILE_PATH_SEPARATOR + "Data"
